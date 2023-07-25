52b1e4bfcf1377e4e9284b46137098ca13671664
mlnbk <dev.milan.bako@gmail.com>
2023-07-25 12:18:31 +0200

    feat: add chart
    * add Chart related Widgets
    * add Expense summary class with alternative constructor

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -34,3 +34,27 @@ class Expense {
     return DateFormat.yMd().format(date);
   }
 }
+
+class ExpenseSummary {
+  const ExpenseSummary({
+    required this.category,
+    required this.expenses,
+  });
+
+  // custom alternative constructor
+  ExpenseSummary.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+    return sum;
+  }
+}
